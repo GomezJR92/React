dcd068b1dd753c9b2896c4c8763c49c479703cb9
GomezJR92 <n.albertogomez@gmail.com>
2022-11-08 20:25:48 -0300

    hoy

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Funcionando </h1><a href="https://www.gifsanimados.org/cat-computadora-y-ordenador-56.htm"><img src="https://www.gifsanimados.org/data/media/56/computadora-y-ordenador-imagen-animada-0019.gif" border="0" alt="computadora-y-ordenador-imagen-animada-0019" /></a>
+        <h2>Cuando no funciona el codigo</h2><a href="https://www.gifsanimados.org/cat-computadora-y-ordenador-56.htm"><img src="https://www.gifsanimados.org/data/media/56/computadora-y-ordenador-imagen-animada-0004.gif" border="0" alt="computadora-y-ordenador-imagen-animada-0004" /></a>    
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
